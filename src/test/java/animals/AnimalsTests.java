f545543e5c8260d45522edb44a1609d44a4aba96
Zholudiev Dmytro <dmizho@qamadness.com>
2023-07-26 13:15:07 +0300

    update tests

--- src/test/java/animals/AnimalsTests.java
+++ src/test/java/animals/AnimalsTests.java
@@ -1,10 +1,38 @@
 package animals;
 
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.chrome.ChromeOptions;
+import static com.codeborne.selenide.Selenide.open;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AnimalsTests {
+
+    @BeforeAll
+    public static void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--incognito");
+//        options.addArguments("--disable-gpu");
+//        options.addArguments("enable-automation");
+//        options.addArguments("--headless");
+//        options.addArguments("--window-size=1920,1080");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-extensions");
+//        options.addArguments("--dns-prefetch-disable");
+//        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+//
+//        Configuration.browserCapabilities = options;
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.timeout = 15000;
+//        Configuration.headless = false;
+
+        open("https://google.com/");
+    }
 
     @Test
     public void testLucky() {
