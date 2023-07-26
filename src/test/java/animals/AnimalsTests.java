347dd07784be1173ace5990599bd907f85e2483c
Zholudiev Dmytro <dmizho@qamadness.com>
2023-07-26 21:13:42 +0300

    update chrome settings

--- src/test/java/animals/AnimalsTests.java
+++ src/test/java/animals/AnimalsTests.java
@@ -15,21 +15,21 @@ public class AnimalsTests {
     @BeforeAll
     public static void setupDriver() {
         WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--incognito");
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("enable-automation");
-//        options.addArguments("--headless");
-//        options.addArguments("--window-size=1920,1080");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-extensions");
-//        options.addArguments("--dns-prefetch-disable");
-//        options.setPageLoadStrategy(PageLoadStrategy.NONE);
-//
-//        Configuration.browserCapabilities = options;
-//        Configuration.browserSize = "1920x1080";
-//        Configuration.timeout = 15000;
-//        Configuration.headless = false;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--disable-gpu");
+        options.addArguments("enable-automation");
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--dns-prefetch-disable");
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+
+        Configuration.browserCapabilities = options;
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 15000;
+        Configuration.headless = true;
 
         open("https://google.com/");
     }
